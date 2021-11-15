70c4b67d9b443c8345b76b1fe56acc5eeb54e968
Abhay TOMAR <abhay.tomar@aveva.com>
2021-11-15 20:00:29 +0530

    escape ley close user popup
    escape ley close user popup

--- src/app/modules/shared/components/user-popup/acme-sc-user-popup.component.ts
+++ src/app/modules/shared/components/user-popup/acme-sc-user-popup.component.ts
@@ -2,6 +2,8 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { AcmeSCAuthorizationService } from '../../../../core/services/acme-sc-authorization.service';
 import { ILoginEntity } from '../../../../core/entities/acme-sc-login.entity';
 
+import { HostListener } from '@angular/core';
+
 @Component({
     selector: 'acme-sc-user-popup',
     templateUrl: './acme-sc-user-popup.component.html',
@@ -12,7 +14,7 @@ export class AcmeSCUserPopupComponent {
 
     @Output() closed = new EventEmitter();
     @Output() logOut = new EventEmitter();
-    
+
     constructor(private acmeSCAuthorizationService: AcmeSCAuthorizationService) {
         this.loginEntity = this.acmeSCAuthorizationService.getSession();
     }
@@ -23,5 +25,12 @@ export class AcmeSCUserPopupComponent {
 
     userLogOut() {
         this.logOut.emit();
+    }
+
+    @HostListener('document:keydown.escape', ['$event'])
+    handleKeyboardEvent(event: KeyboardEvent) {
+        if(event.key === 'Escape') {
+            this.closed.emit();
+        }
     }
 }
