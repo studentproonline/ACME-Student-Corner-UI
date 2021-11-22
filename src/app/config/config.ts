6f61ea130b974256b7936502673e0c31c532f0a1
Abhay TOMAR <abhay.tomar@aveva.com>
2021-11-22 18:23:30 +0530

    Adding beta label to conference app menu item
    Adding beta label to conference app menu item

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,4 +1,4 @@
 export const apiConfig = {
-    url: 'http://localhost:3000/api/v1'
-   //url: 'https://acme-student-corner-api.herokuapp.com/api/v1'
+    //url: 'http://localhost:3000/api/v1'
+    url: 'https://acme-student-corner-api.herokuapp.com/api/v1'
 };
