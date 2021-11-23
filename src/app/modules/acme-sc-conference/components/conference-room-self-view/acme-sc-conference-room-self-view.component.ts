06b92725005cf90041f62ace89e11d70f8719134
Abhay TOMAR <abhay.tomar@aveva.com>
2021-11-23 15:30:58 +0530

    added support for touch events to drag self view in conference
    added support for touch events to drag self view in conference

--- src/app/modules/acme-sc-conference/components/conference-room-self-view/acme-sc-conference-room-self-view.component.ts
+++ src/app/modules/acme-sc-conference/components/conference-room-self-view/acme-sc-conference-room-self-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ElementRef,HostListener, ViewChild } from '@angular/core';
+import { Component, Input, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'acme-sc-conference-room-self-view',
@@ -9,19 +9,18 @@ export class AcmeSCConferenceRoomSelfViewComponent {
     @Input() localCallId: any;
     @Input() userName: any;
     startDrag: boolean;
-    topStart:number=0;
-    leftStart:number=0;
+    topStart: number = 0;
+    leftStart: number = 0;
 
-    @ViewChild('viewcard', {static: false, read: ElementRef}) viewCard: ElementRef;
+    @ViewChild('viewcard', { static: false, read: ElementRef }) viewCard: ElementRef;
     // @ViewChild('viewCard') viewCard!: ElementRef;
     //ViewChildren('viewCard') viewCard: any;
 
-    constructor(public element: ElementRef)
-    {
+    constructor(public element: ElementRef) {
 
     }
 
-    ngAfterViewInit(){
+    ngAfterViewInit() {
         console.log(this.viewCard);
     }
 
@@ -29,11 +28,11 @@ export class AcmeSCConferenceRoomSelfViewComponent {
     onMouseDown(event: MouseEvent) {
         if (event.button === 2)
             return; // prevents right click drag, remove his if you don't want it
-       
+
         this.topStart = event.clientY - this.viewCard.nativeElement.offsetTop;
         this.leftStart = event.clientX - this.viewCard.nativeElement.offsetLeft;
         this.startDrag = true;
-     }
+    }
 
 
     @HostListener('document:mousemove', ['$event'])
@@ -47,5 +46,28 @@ export class AcmeSCConferenceRoomSelfViewComponent {
     @HostListener('document:mouseup', ['$event'])
     onMouseUp(event: MouseEvent) {
         this.startDrag = false;
+    }
+
+    // touch events for touch devices
+    @HostListener('touchstart', ['$event'])
+    onTouchStart(event: TouchEvent) {
+        this.topStart = event.changedTouches[0].clientY - this.viewCard.nativeElement.offsetTop;
+        this.leftStart = event.changedTouches[0].clientX - this.viewCard.nativeElement.offsetLeft;
+        this.startDrag = true;
+        event.stopPropagation();
+    }
+
+    @HostListener('document:touchend')
+    onTouchEnd() {
+        this.startDrag = false;
+    }
+
+    @HostListener('document:touchmove', ['$event'])
+    onTouchMove(event: TouchEvent) {
+        if (this.startDrag) {
+            this.viewCard.nativeElement.style.top = (event.changedTouches[0].clientY  - this.topStart) + 'px';
+            this.viewCard.nativeElement.style.left = (event.changedTouches[0].clientX - this.leftStart) + 'px';
+        }
+        event.stopPropagation();
     }
 }
